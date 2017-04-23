928cb777db55dcdc73ce2f74b5a361d87df08e49
Paul-Yves <lucas.paulyves@gmail.com>
2017-04-23 19:54:30 +0200

    setup basic router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
 import logo from './wjdr_logo.png';
 import './App.css';
 import NameGenerator from './components/NameGenerator.js';
@@ -6,13 +8,22 @@ import NameGenerator from './components/NameGenerator.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="Warhammer jdr toolbox" />
-          <h2>Welcome to Warhammer jdr toolbox</h2>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="Warhammer jdr toolbox" />
+            <h2>Welcome to Warhammer jdr toolbox</h2>
+          </div>
+          <div className="nav">
+            <ul>
+              <li><NavLink activeClassName="current" to="/" exact={true} >Random PNJ</NavLink></li>
+              <li><NavLink activeClassName="current" to="/criticals">Critiques</NavLink></li>
+              <li><NavLink activeClassName="current" to="/trade">Commerce</NavLink></li>
+            </ul>
+          </div>
+          <Route exact path="/" component={NameGenerator}/>
         </div>
-        <NameGenerator/>
-      </div>
+      </Router>
     );
   }
 }
